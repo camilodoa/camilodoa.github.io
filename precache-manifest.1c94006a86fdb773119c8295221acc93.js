7ebdde8bac5b9707f10cf0262a90abdee797245a
camilodoa <camilodortiz@gmail.com>
2020-02-12 19:41:00 -0500

    Updates

---
+++ precache-manifest.1c94006a86fdb773119c8295221acc93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dffec60b21641f808f47785cb8a60ff3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3312b481c0bc4878b36c",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "4576e2fea6d36ca74836",
+    "url": "/static/css/main.abe6196f.chunk.css"
+  },
+  {
+    "revision": "3312b481c0bc4878b36c",
+    "url": "/static/js/2.a1f4c4f3.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.a1f4c4f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4576e2fea6d36ca74836",
+    "url": "/static/js/main.18646b5f.chunk.js"
+  },
+  {
+    "revision": "060123f01d46cf428d47",
+    "url": "/static/js/runtime-main.805ac67f.js"
+  }
+]);
