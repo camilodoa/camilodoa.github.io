e64e0022b6b5781290908d62e27cba9fa133d377
camilo ortiz <camilodortiz@gmail.com>
2020-05-08 22:24:42 -0400

    cleanup code

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,16 @@ import {Switch, Route} from 'react-router-dom';
 function App() {
   return (
     <div>
-      <div>
         <Header/>
-      </div>
-      <div>
         <Switch>
-          <Route exact path='/'>
-            <Home/>
-          </Route>
-          <Route path='/photos'>
-            <PhotoStream/>
-          </Route>
+            <Route exact path='/'>
+                <Home/>
+            </Route>
+            <Route path='/photos'>
+                <PhotoStream/>
+            </Route>
         </Switch>
-      </div>
-      <div>
         <Footer/>
-      </div>
     </div>
   );
 }
